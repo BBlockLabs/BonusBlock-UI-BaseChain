0346be4c2a5f7d35c5392ebb322835a8cd145a30
Johan Laas <johan.laas@vntrs.com>
2024-06-27 15:24:19 +0300

    change clashing classNames

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -11,20 +11,20 @@ const Accordion = () => {
     return (
         <RadixAccordion.Root className="w-full flex flex-col gap-7" type="single" collapsible>
             {accordionItems.map((item) => (
-                <RadixAccordion.Item className="item shadow" value={item} key={item}>
+                <RadixAccordion.Item className="accordion-item shadow" value={item} key={item}>
                     <RadixAccordion.Trigger
-                        className="trigger"
+                        className="accordion-trigger"
                     >
                         <div className="flex items-center gap-[58px]">
-                            <CashIcon className="trigger-icon w-10 h-auto" />
+                            <CashIcon className="accordion-trigger-icon w-10 h-auto" />
                             <span className="text-white text-[25px]/[30px]">{item}</span>
                         </div>
-                        <div className="contain flex items-center gap-2.5">
+                        <div className="accordion-trigger-contain flex items-center gap-2.5">
                             <span className="text-white text-[30px]/[48px]">0/25</span>
                             <Triangle className="triangle w-4 h-4" />
                         </div>
                     </RadixAccordion.Trigger>
-                    <RadixAccordion.Content className="content AccordionChevron accordion-content">
+                    <RadixAccordion.Content className="AccordionChevron accordion-content">
                         {Array.from({ length: 6 }).map((_, i) => (
                             <QuestCard key={i} />
                         ))}
